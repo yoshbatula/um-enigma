200983b574ec7fa3b61a4e3e000c7bb7b1c236e3
Fuz7 <124787267+Fuz7@users.noreply.github.com.>
2025-01-06 22:57:08 +0800

    Finish Hero Section Animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -15,9 +15,35 @@ export default function Hero() {
       <p className="font-Poppins-Medium text-[32px] mb-[5px] ml-[20px]">
         Ignite Aspiration. Spark Excellence.
       </p>
-      <p className="text-[360px] font-Poppins-Bold leading-[0.8] ">ENIGMA
-        
-      </p>
+      <div className="min-h-auto w-fit relative leading-[0.9] mb-[20px] overflow-clip ">
+        <span className="font-Poppins-Bold text-[20vw] invisible">ENIGMA</span>
+        <svg className="absolute top-0 left-0 w-full h-full">
+          <mask className="" id="textMask">
+          <text
+            className="relative"
+            fill="white"
+            x="0"
+            y="0"
+            fontSize={"20vw"}
+            fontFamily="Poppins-Bold"
+            dominantBaseline={"Hanging"}
+          >
+            ENIGMA
+          </text>
+
+          </mask>
+        </svg>
+        <span className="absolute text left-0  w-[100%] h-full bg-[white]">
+
+        <span className="absolute bg-neonGradient -rotate-[30deg] rotate neonGradient left-[-10%] 
+         top-[-100%] w-[35%] h-[300%] 
+        "></span>
+        <span className="absolute bg-crimsonGradient rounded-full aspect-square w-[60%]
+        translate-y-full  -translate-x-[50%]"></span>
+        <span className="absolute bg-crimsonGradient2 rounded-full aspect-square w-[75%]
+           -translate-y-2/4 translate-x-[90%]"></span>
+        </span>
+      </div>
     </section>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,22 @@ export default {
       backgroundImage: {
         heroGradient:
           "linear-gradient(180deg, rgba(12,98,145,1) 5%, rgba(24,24,24,1) 40%, rgba(0,0,0,1) 100%)",
+        neonGradient:
+         " linear-gradient(90deg, rgba(170,195,209,0.1) 0%,rgba(26,107,151,1) 50%,  rgba(170,195,209,0.1) 100%);",
+        crimsonGradient:
+        'radial-gradient(circle, rgba(131,34,77,1) 0%, rgba(131,34,77,1) 23%, rgba(255,255,255,0.000001) 70%)',
+        crimsonGradient2:
+        'radial-gradient(circle, rgba(131,34,77,1) 0%, rgba(131,34,77,1) 15%, rgba(255,255,255,0.000001) 70%)'
+        },
+
+      fontFamily: {
+        "Poppins-Light": "Poppins-Light",
+        "Poppins-Regular": "Poppins-Regular",
+        "Poppins-Medium": "Poppins-Medium",
+        "Poppins-SemiBold": "Poppins-SemiBold",
+        "Poppins-Bold": "Poppins-Bold",
+        "Poppins-Black": "Poppins-Black",
       },
-      fontFamily:{
-        'Poppins-Light':'Poppins-Light',
-        'Poppins-Regular':'Poppins-Regular',
-        'Poppins-Medium':'Poppins-Medium',
-        'Poppins-SemiBold':'Poppins-SemiBold',
-        'Poppins-Bold':'Poppins-Bold',
-        'Poppins-Black':'Poppins-Black',
-      }
     },
   },
   plugins: [],
