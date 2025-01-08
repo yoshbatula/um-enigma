2657ec476a83c4ea26a2ed706a1c2064625ddfe3
Fuz7 <124787267+Fuz7@users.noreply.github.com.>
2025-01-08 22:42:23 +0800

    Finish logo section BlueRectangle animation

--- src/components/Logo/Animations.jsx
+++ src/components/Logo/Animations.jsx
@@ -1,11 +1,11 @@
 import { motion } from "motion/react"
-
+import blueRectangle from '@images/blueRectangle.svg'
 
 export default function Animations(){
   return (
     <>
     <RedBox />
-    <div className='w-full'></div>
+    <BlueRectangle />
     <div className='w-full'></div>
     <div className='w-full'></div>
   </>)
@@ -13,8 +13,8 @@ export default function Animations(){
 
 const RedBox = ()=>{
   return(<>
-    <div className='w-full py-[10px]'>
-    <div className='w-[60%] -rotate-45  grid grid-cols-2 grid-rows-2 gap-[30px]'>
+    <div className='w-full pt-[30px]'>
+    <div className='w-[65%] -rotate-45  grid grid-cols-2 grid-rows-2 gap-[30px]'>
       <motion.div 
       animate={{
         x:'calc(100%  + 30px)'
@@ -75,4 +75,43 @@ const RedBox = ()=>{
 
     </div>
   </>)
+}
+
+const BlueRectangle = () =>{
+  return(
+    <>
+    <div className='w-full  h-fit flex gap-[20px] items-start overflow-hidden'>
+    
+    <motion.img
+      animate={{
+        y:'101%'
+      }}
+      transition={{
+        type:'spring',
+        stiffness: 100, // Adjust stiffness for movement speed
+        damping: 15, // Adjust damping to control how "bouncy" the animation is
+        mass: 1, // Set mass to modify the weight of the movement
+        repeatDelay:1,
+        repeatType:'mirror',
+        repeat:Infinity
+      }}
+     className="max-w-[calc(50%_-_10px)]" src={blueRectangle} alt="" />
+    <motion.img 
+      animate={{
+        y:'-101%',
+        scaleX:[-1,-1]
+      }}
+      transition={{
+        type:'spring',
+        stiffness: 100, // Adjust stiffness for movement speed
+        damping: 15, // Adjust damping to control how "bouncy" the animation is
+        mass: 1, // Set mass to modify the weight of the movement
+        repeatDelay:1,
+        repeatType:'mirror',
+        repeat:Infinity
+      }}
+    className="-scale-x-[1] max-w-[calc(50%_-_10px)]" src={blueRectangle} alt="" />
+    </div>
+    </>
+  )
 }
