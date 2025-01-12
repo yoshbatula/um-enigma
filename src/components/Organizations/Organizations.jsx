7a42a585ee6c534b9f575c1b4eed6dfa1e7ebfe6
Fuz7 <124787267+Fuz7@users.noreply.github.com.>
2025-01-12 23:15:55 +0800

    Make organization section responsive

--- src/components/Organizations/Organizations.jsx
+++ src/components/Organizations/Organizations.jsx
@@ -10,19 +10,19 @@ import org8 from '@images/org8.svg'
 
 export default function Organizations(){
   return(
-    <section className="min-h-[68vh] grid grid-rows-[40%_60%] mb-[30px] p-[30px] px-[calc(8vw_+_30px)]">
-      <div className="flex justify-between items-center">
+    <section className="min-h-[100vh] h-fit flex flex-col gap-[12px] lg:gap-0 lg:grid lg:min-h-[68vh]  lg:grid-rows-[40%_60%] mb-[30px] px-[4vw] lg:px-[6vw]  xl:p-[30px] xl:px-[calc(8vw_+_30px)]">
+      <div className="flex justify-between   items-start lg:items-center px-[20px] lg:px-0">
         <div className="flex flex-col">
-          <div className='flex items-center gap-[12px]'>
+          <div className='flex items-center gap-[12px] mb-[12px]'>
             <div className='w-[40px] h-[2px] bg-[#A63346]'></div>
-            <h3 className='font-Poppins-Medium text-[18px] text-[#A63346] mb-[12px]'>Tech Organizations</h3>
+            <h3 className='font-Poppins-Medium text-[16px] lg:text-[18px] text-[#A63346] '>Tech Organizations</h3>
           </div>
-            <h4 className='font-Poppins-SemiBold text-[43px] text-[#2C2C2C] mb-[16px]'>COMMUNITIES</h4>
-            <p className='font-Poppins-Medium text-[18px] text[#2C2C2C] opacity-70'>Student tech organizations affiliated by Enigma</p>
+            <h4 className='font-Poppins-SemiBold text-[clamp(24px,3.8vw,43px)] text-[#2C2C2C] mb-[16px]'>COMMUNITIES</h4>
+            <p className='font-Poppins-Medium text-[clamp(14px,2vw,18px)] text[#2C2C2C] opacity-70'>Student tech organizations affiliated by Enigma</p>
         </div>
-        <img className='aspect-square w-[120px]' src={organizationArrow} alt="" />
+        <img className='aspect-square w-[60px] lg:w-[120px]' src={organizationArrow} alt="" />
       </div>
-      <div className='grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] grid-rows-2 mt-[45px]'>
+      <div className='grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] grid-rows-2 gap-y-[48px] lg:gap-0 mt-[45px]'>
         <Images />
       </div>
     </section>
